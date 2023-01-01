b739095b070a27d8a487660b144c162c3fe92321
shahar.ben <shahar.ben@campus.technion.ac.il>
2023-01-01 15:40:44 +0200

    started hashing

---
+++ hash_table.h
@@ -0,0 +1,15 @@
+//
+// Created by shahar on 01/01/2023.
+//
+
+#ifndef WET2_NEW_HASH_TABLE_H
+#define WET2_NEW_HASH_TABLE_H
+
+#include "chain_Node.h"
+#include "player.h"
+
+class hash_table{
+    
+};
+
+#endif //WET2_NEW_HASH_TABLE_H

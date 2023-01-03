4161612448a298c4dca444590317636ee3afabbc
Idan Hermelin <idanhermelin@campus.technion.ac.il>
2023-01-03 21:39:16 +0200

    Update Union_Find.h

--- Union_Find.h
+++ Union_Find.h
@@ -12,9 +12,9 @@ class Union_Find
 public:
     Union_Find(int size, int pow);
     ~Union_Find() = default;
-    Node<player>* MakeSet(int playerId);
     Node<player>* Find (int player_id);
     Node<player>* Union (Node<player>* group1, Node<player>* group2);
+    hash_table get_hashed_array() const;
 };
 
 #endif //WET2_NEW_UNION_FIND_H

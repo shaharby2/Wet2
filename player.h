d286dde511c3c0eeaca4e6ef16ad91585e5a8dc9
shaharby2 <102796990+shaharby2@users.noreply.github.com>
2023-01-07 21:20:45 +0200

    Update player.h

--- player.h
+++ player.h
@@ -17,7 +17,7 @@ private:
     int m_ability;
     int m_team_games;
     int m_size_of_team;
-    int m_goalkeeper;
+    bool m_goalkeeper;
     bool m_is_legal;
     permutation_t m_partial_spirit;
     permutation_t m_root_spirit;
@@ -33,7 +33,7 @@ public:
     int get_ability() const;
     int get_team_games() const;
     int get_size_of_team() const;
-    int get_goal_keeper() const;
+    bool get_goal_keeper() const;
     bool get_is_legal() const;
     permutation_t get_partial_spirit() const;
     permutation_t get_root_spirit() const;
@@ -45,7 +45,7 @@ public:
     void set_team_games(int games);
     void set_size_of_team(int size);
     void set_is_legal(bool is_legal);
-    void set_goal_keeper(int goal_keeper);
+    void set_goal_keeper(bool goal_keeper);
     void set_partial_spirit(const permutation_t& perm);
     void set_root_spirit(const permutation_t& perm);
     void update_members(int games_played, int cards);
